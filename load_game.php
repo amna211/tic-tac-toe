59744bac740d41755295fdc11f87da375e30b93c
BSCE21018 <bsce21018@itu.edu.pk>
2025-02-01 00:21:45 +0500

    tic-tac-toe

--- load_game.php
+++ load_game.php
@@ -5,3 +5,10 @@ $stmt = $pdo->query('SELECT * FROM games ORDER BY id DESC LIMIT 1');
 $game = $stmt->fetch();
 echo json_encode(json_decode($game['board'], true));
 ?>
+<?php
+include 'db.php';
+
+$stmt = $pdo->query('SELECT * FROM games ORDER BY id DESC LIMIT 1');
+$game = $stmt->fetch();
+echo json_encode(json_decode($game['board'], true));
+?>
